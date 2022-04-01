843980d32bddd1385b9934df7ea0da45e82eacb2
Thomas <tommyprezioso@gmail.com>
2022-04-01 17:25:18 -0400

    Initial Commit

---
+++ Status of Github Menu App/Status_of_Github_Menu_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Status_of_Github_Menu_AppApp.swift
+//  Status of Github Menu App
+//
+//  Created by Thomas Prezioso Jr on 4/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Status_of_Github_Menu_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
